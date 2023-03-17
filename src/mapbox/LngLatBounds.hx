53375eda57a02d5c2178ec4274394a490c2f800b
daniellsl <laushunleung18@yahoo.com.hk>
2023-03-17 11:53:08 +0800

    Update LngLatBounds.hx
    Add `isEmpty()` method for preventing null case of `toArray()`
    
    Refs:
    
    https://github.com/mapbox/mapbox-gl-js/pull/5917
    
    https://docs.mapbox.com/mapbox-gl-js/api/geography/#lnglatbounds#isempty

--- src/mapbox/LngLatBounds.hx
+++ src/mapbox/LngLatBounds.hx
@@ -3,6 +3,7 @@ package mapbox;
 @:jsRequire('mapbox-gl', 'LngLatBounds')
 extern class LngLatBounds {
 	function new(?sw:LngLatLike, ?ne:LngLatLike):Void;
+	function isEmpty():Bool;
 	function extend(?obj:LngLatLike):LngLatBounds;
 	function toArray():Array<Array<Float>>;
 }
